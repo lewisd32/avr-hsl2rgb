c2e6f921702ae524be617c67f3df85e264ff813d
Derek Lewis <github@lewisd.com>
2012-07-20 16:45:52 +0100

    Adding comments.

--- hsl2rgb.h
+++ hsl2rgb.h
@@ -3,7 +3,10 @@
 
 #include <inttypes.h>
 
+// Convert HSL to RGB using the fast algorithm
 void hsl2rgb(uint16_t hue, uint8_t sat, uint8_t lum, uint8_t rgb[3]);
+
+// Convert HSL to RGB using the original algorithm
 void hsl2rgb_orig(uint16_t hue, uint8_t sat, uint8_t lum, uint8_t rgb[3]);
 
 #endif
